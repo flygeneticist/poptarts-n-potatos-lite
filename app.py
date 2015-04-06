e0ccdb0166132f6318ef79b797cf12bc2c2eec71
Kevin Keller <flygeneticist@gmail.com>
2015-04-06 12:48:44 +0700

    placeholder vars for blog content added

--- app.py
+++ app.py
@@ -15,6 +15,7 @@ def home():
 
 @app.route('/blog/<post>')
 def blog_post(post):
+    post = {'title': 'Post #1', 'url': 'post-1', 'summary':'This is a cool post about...', 'content': '<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo onsequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>'}
     return render_template('blogPost.html', post=post)
 
 @app.route('/admin')

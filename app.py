e4adc359fbb1672242b285b4b4a1bd491034ac79
Kevin Keller <flygeneticist@gmail.com>
2015-04-09 15:48:25 +0700

    Login form class and template added.

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
 from flask import Flask, request, redirect, render_template
 from flask.ext.wtf import Form
-from wtforms import StringField, SubmitField
+from wtforms import StringField, PasswordField, SubmitField
 from wtforms.validators import Required
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = "hahahahahaha"
 
 # hooks for routes
 
@@ -31,7 +32,8 @@ def admin_dash():
 
 @app.route('/admin/login')
 def login():
-    return 'Login page for admin dashboard'
+    form = LoginForm()
+    return render_template('login.html', form=form)
 
 # error handlers
 @app.errorhandler(404)
@@ -41,6 +43,12 @@ def page_not_found(e):
 @app.errorhandler(500)
 def internal_server_error(e):
     return render_template('500.html'), 500
+
+# Login Form logic using WTF
+class LoginForm(Form):
+    email = StringField('Email', validators=[Required()])
+    password = PasswordField('Password', validators=[Required()])
+    submit = SubmitField('Submit')
 
 # run the app
 if __name__ == '__main__':
